aceb3d110bab537e5ada64fef4dc4af92ef8c722
Rpessoa <rpessoa.mobile@gmail.com>
2017-04-09 11:24:42 -0400

    Changed return String to Boolean

--- src/pivottablefinal/Readuser.java
+++ src/pivottablefinal/Readuser.java
@@ -7,29 +7,32 @@ import javax.swing.JFrame;
 public class Readuser {
 
 // Use the username to read the file with the password
-public static void readUserProfile(String userName, String inputedPwd) {
-
-  try {
-			File file = new File(userName + ".txt");
-			FileReader fileReader = new FileReader(file);
-			BufferedReader bufferedReader = new BufferedReader(fileReader);
-			StringBuffer stringBuffer = new StringBuffer();
-			String line;
-			while ((line = bufferedReader.readLine()) != null) {
-				stringBuffer.append(line);
-//				stringBuffer.append("\n");
-			}
-			fileReader.close();
-                        
-                        if(inputedPwd.equals(stringBuffer.toString())) {
-                            System.out.println("PWD Checked!!!");
-                        } else {
-                            System.out.println("Not the same PWD!!!");
-                        }
-			System.out.println(userName + "'s pwd:");
-			System.out.println(stringBuffer.toString());
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+public static boolean readUserProfile(String userName, String inputedPwd) {
+    
+    try {
+        File file = new File(userName + ".txt");
+        FileReader fileReader = new FileReader(file);
+        BufferedReader bufferedReader = new BufferedReader(fileReader);
+        StringBuffer stringBuffer = new StringBuffer();
+        String line;
+        while ((line = bufferedReader.readLine()) != null) {
+            stringBuffer.append(line);
+//            stringBuffer.append("\n");
+	}
+        fileReader.close();
+        
+        if(inputedPwd.equals(stringBuffer.toString())) {
+            System.out.println("PWD Checked!!!");
+            return true;
+        } else {
+            System.out.println("Not the same PWD!!!");
+            return false;
+        }
+//			System.out.println(userName + "'s pwd:");
+//			System.out.println(stringBuffer.toString());
+	} catch (IOException e) {
+            e.printStackTrace();
+            return false;
+        }
   }
 }
