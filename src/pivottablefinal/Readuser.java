a06d7cb046b5660c2f4651a3a40f319d4df70c1b
Rpessoa <rpessoa.mobile@gmail.com>
2017-04-09 10:38:19 -0400

    Updated Hash and PWD check

---
+++ src/pivottablefinal/Readuser.java
@@ -0,0 +1,35 @@
+// Created by Rodrigo
+// 02/04/2017
+
+import java.io.*;
+import javax.swing.JFrame;
+
+public class Readuser {
+
+// Use the username to read the file with the password
+public static void readUserProfile(String userName, String inputedPwd) {
+
+  try {
+			File file = new File(userName + ".txt");
+			FileReader fileReader = new FileReader(file);
+			BufferedReader bufferedReader = new BufferedReader(fileReader);
+			StringBuffer stringBuffer = new StringBuffer();
+			String line;
+			while ((line = bufferedReader.readLine()) != null) {
+				stringBuffer.append(line);
+//				stringBuffer.append("\n");
+			}
+			fileReader.close();
+                        
+                        if(inputedPwd.equals(stringBuffer.toString())) {
+                            System.out.println("PWD Checked!!!");
+                        } else {
+                            System.out.println("Not the same PWD!!!");
+                        }
+			System.out.println(userName + "'s pwd:");
+			System.out.println(stringBuffer.toString());
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+  }
+}
